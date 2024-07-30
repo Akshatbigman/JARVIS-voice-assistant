c1263f0f5ff7ebbc1ada19c7b0b825343e85e480
Akshat Sharma <akshanik1104@gmail.com>
2024-07-30 11:30:04 +0400

    Able to go through the internet
    still needs to be connected through flask to html

---
+++ Jarvis/__init__.py
@@ -0,0 +1,76 @@
+import speech_recognition as sr
+import pyttsx3
+
+from Jarvis.features import date_time
+from Jarvis.features import launch_app
+from Jarvis.features import website_open
+from Jarvis.features import system_stats
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[0].id)
+
+class JarvisAssistant:
+    def __init__(self):
+        pass
+
+    def mic_input(self):
+        """
+        Fetch input from mic
+        return: user's voice input as text if true, false if fail
+        """
+        try:
+            r = sr.Recognizer()
+            # r.pause_threshold = 1
+            # r.adjust_for_ambient_noise(source, duration=1)
+            with sr.Microphone() as source:
+                print("Listening....")
+                r.energy_threshold = 4000
+                audio = r.listen(source)
+            try:
+                print("Recognizing...")
+                command = r.recognize_google(audio, language='en-in').lower()
+                print(f'You said: {command}')
+            except:
+                print('Please try again')
+                command = self.mic_input()
+            return command
+        except Exception as e:
+            print(e)
+            return  False
+
+
+    def tts(self, text):
+        """
+        Convert any text to speech
+        :param text: text(String)
+        :return: True/False (Play sound if True otherwise write exception to log and return  False)
+        """
+        try:
+            engine.say(text)
+            engine.runAndWait()
+            engine.setProperty('rate', 175)
+            return True
+        except:
+            t = "Sorry I couldn't understand and handle this input"
+            print(t)
+            return False
+
+    def tell_me_date(self):
+
+        return date_time.date()
+
+    def tell_time(self):
+
+        return date_time.time()
+
+    def website_opener(self, domain):
+        """
+        This will open website according to domain
+        :param domain: any domain, example "youtube.com"
+        :return: True if success, False if fail
+        """
+        return website_open.website_opener(domain)
+
+
